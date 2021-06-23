c5e79429f20780998cef31a99a1a23ed7545ac19
Nam Huynh <namhq.1989@gmail.com>
2021-06-23 20:54:55 +0700

    update

--- queue/model/notification.go
+++ queue/model/notification.go
@@ -1,8 +1,19 @@
 package queuemodel
 
+import (
+	"firebase.google.com/go/v4/messaging"
+	"github.com/Otobook-vn/modules/model"
+)
+
 // NotificationCreateAndPushSingle ...
 type NotificationCreateAndPushSingle struct {
-	ID     string
-	FieldA string
-	FieldB string
+	Notification model.Notification
+	IsPush       bool
+}
+
+// NotificationPushByTokens ...
+type NotificationPushByTokens struct {
+	BatchID string
+	Tokens  []string
+	Message messaging.Message
 }
