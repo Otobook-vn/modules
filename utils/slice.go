f9b78abc16f9210583e9c364e5cefbb4c505c48e
Nam Huynh <namhq.1989@gmail.com>
2021-07-01 11:15:44 +0700

    update

---
+++ utils/slice.go
@@ -0,0 +1,11 @@
+package utils
+
+// SliceIndex ...
+func SliceIndex(limit int, predicate func(i int) bool) int {
+	for i := 0; i < limit; i++ {
+		if predicate(i) {
+			return i
+		}
+	}
+	return -1
+}
