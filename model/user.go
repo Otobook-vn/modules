6e4b0ebb8e6c6382a247aab16a92221518e08106
Nam Huynh <namhq.1989@gmail.com>
2021-07-01 22:42:37 +0700

    update

--- model/user.go
+++ model/user.go
@@ -22,7 +22,6 @@ type User struct {
 	Status           string
 	ReferralCode     string
 	Desc             string
-	ConsultantRating float64
 	LocationAddress  string
 	SearchTokens     TsVector
 	IsPhoneVerified  bool
--- model/user_stats.go
+++ model/user_stats.go
@@ -2,15 +2,16 @@ package model
 
 // UserStats ...
 type UserStats struct {
-	ID              string `gorm:"primaryKey"`
-	Post            int
-	CurrentCoin     int
-	Reward          int
-	Car             int
-	Follower        int
-	Following       int
-	ReferralTotal   int
-	ReferralSuccess int
+	ID               string `gorm:"primaryKey"`
+	Post             int
+	CurrentCoin      int
+	Reward           int
+	Car              int
+	Follower         int
+	Following        int
+	ReferralTotal    int
+	ReferralSuccess  int
+	ConsultantRating float64
 
 	// Ref
 	User   User `gorm:"foreignKey:UserID"`
